bde2d6fc9000d037b1730e69b1eea40e64446f4c
juandrengifo <juan.rengifo912@gmail.com>
2019-04-17 15:34:42 -0500

    Se dejará de trabajar con una interfaz con el objetivo de manejar una sola clase que varía los resultados de sus operaciones dependiendo de la sucursal con que sea inicializada

--- app.scala
+++ app.scala
@@ -1,32 +1,20 @@
 import scala.io.Source
 import java.io.PrintWriter
 
-
-trait Sucursal{
-  private var _precioDomicilio : Double = _
-  private var _historial : List[(String, Double, Double)] = _  //(Fecha, Ganancias, Gastos)
-  private var _sede : String = _
-  //private var _insumos: List[Insumo] = _
-
-
-  //Setters y getters
-  def getPrecioDomicilio() : Double
-  def setPrecioDomicilio(precio : Double) : Unit
-  def getHistorial() : List[(String, Double, Double)]
-  def setHistorial(hist : List[(String, Double, Double)]) : Unit
-  //def setInsumos(ins : List[Insumo]) : Unit
-  //def getInsumos() : List[Insumo]
-  def getSede() : String
-
-  //metodos
-  def obtenerUtilerias(fecha : String) : (Boolean, Double)
-}
-
-class sucursalIngenio extends Sucursal{
+class Sucursal(sede : String){ //...insumos : List[Insumo]){
   private var _precioDomicilio : Double = 3000.0
-  private var _historial : List[(String, Double, Double)] = _
-  private var _sede : String = "Ingenio"
+  private var _sede : String = sede
   //private var _insumos: List[Insumo] = _
+
+
+  private var _historial : List[(String, Double, Double)] = Nil
+  val source = Source.fromFile("DB/historiales/"+ _sede + ".txt")
+  val lines = source.getLines
+  while(lines.hasNext){
+    val aux  = lines.next.split(":")
+    _historial = (aux(0), aux(1).toDouble, aux(2).toDouble)::_historial
+  }
+  source.close
 
 
   //Setters y getters
@@ -38,21 +26,18 @@ class sucursalIngenio extends Sucursal{
   //def setInsumos(ins : List[Insumo]) : Unit = _insumos = ins
   //def getInsumos() : List[Insumo] = _insumos
 
+  //Metodos
   def obtenerUtilerias(fecha : String) : (Boolean, Double) = {
-    val source = Source.fromFile("DB/historiales/"+ _sede + ".txt")
-    val lines = source.getLines
     var ok : Boolean = false
     var utilerias : Double = 0
+    val it = Iterator(_historial)
 
-    while(lines.hasNext){
-      val aux  = lines.next.split(":")
-      val line : (String, Double, Double) = (aux(0), aux(1).toDouble, aux(2).toDouble)
-      if(line._1 == fecha){
+    for(registro <- _historial){
+      if(registro._1 == fecha){
         ok = true
-        utilerias = line._2 - line._3
+        utilerias = registro._2-registro._3
       }
     }
-    source.close
 
     return (ok, utilerias)
   }
@@ -61,30 +46,7 @@ class sucursalIngenio extends Sucursal{
 
 object interfaz{
   def main(args : Array[String]){
+    var rest = new Sucursal("Ingenio")
+    println(rest.obtenerUtilerias("4/2/16"))
   }
 }
-/*
-var hist : List[(String, Double, Double)] = Nil
-val source = Source.fromFile("DB/historiales/Ingenio.txt")
-val lines = source.getLines
-println("wvgw")
-
-while(lines.hasNext){
-var aux  = lines.next.split(":")
-hist = (aux(0), aux(1).toDouble, aux(2).toDouble)::hist
-}
-source.close
-
-var rest = new sucursalIngenio
-rest.setHistorial(hist)
-println(rest.obtenerUtilerias("4/2/16")._2)*/
-/*
-val source = Source.fromFile("DB/historialGanancias.txt")
-val files = source.getLines
-
-while(files.hasNext) println(files.next)
-source.close
-
-val pw = new PrintWriter("DB/historialGanancias.txt")
-pw.println("10/3/15:1000432")
-pw.close*/
