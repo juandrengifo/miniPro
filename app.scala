eacb9f20a544e774b390bd8bbb2fed8e4e410775
juandrengifo <juan.rengifo912@gmail.com>
2019-04-20 15:37:43 -0500

    Se creo la base de datos y las operaciones para descargar informacion

--- app.scala
+++ app.scala
@@ -1,39 +1,105 @@
 import scala.io.Source
 import java.io.PrintWriter
 
-class Sucursal(sede : String){
+
+class DB{
+  def descargarInsumos() : List[Insumo] = {
+    var insumos : List[Insumo] = Nil
+    var source = Source.fromFile("DB/insumos.txt")
+    var lines = source.getLines
+    var aux : List[String] = lines.toList
+    for(line <- aux){
+      var parsing = line.split(":")
+      if(parsing.length == 7){
+        var alimento = new Alimento(parsing(0), parsing(3).toDouble, parsing(2).toDouble, parsing(1), parsing(5).toDouble, parsing(4).toDouble, parsing(6))
+        insumos = alimento::insumos
+      }
+      else{
+        var util = new Util(parsing(0), parsing(3).toDouble, parsing(2).toDouble, parsing(1))
+        insumos = util::insumos
+      }
+    }
+    source.close
+
+    return insumos
+  }
+  /*
+  def cargarPedidos(insumos : List[Insumo]) : Boolean = {
+    val pw = new PrintWriter("DB/insumos.txt")
+
+    for(insumo <- insumos){
+      if(insumo.isInstanceOf(Alimento)){
+        pw.println()
+      }
+    }
+    pw.close
+  }
+  */
+
+  def descargarAlimento() : List[Alimento] = {
+    var insumos : List[Alimento] = Nil
+    var source = Source.fromFile("DB/insumos.txt")
+    var lines = source.getLines
+    var aux : List[String] = lines.toList
+    for(line <- aux){
+      var parsing = line.split(":")
+      if(parsing.length == 7){
+        var alimento = new Alimento(parsing(0), parsing(3).toDouble, parsing(2).toDouble, parsing(1), parsing(5).toDouble, parsing(4).toDouble, parsing(6))
+        insumos = alimento::insumos
+      }
+    }
+    source.close
+
+    return insumos
+  }
+
+  def descargarPedidos(nombreSucursal : String) : List[(String, Insumo)] = {
+    var pedidos : List[(String, Insumo)] = Nil
+    var source = Source.fromFile("DB/pedidos/" + nombreSucursal + ".txt")
+    var lines = source.getLines
+    var aux : List[String] = lines.toList
+    for(line <- aux){
+      var parsing = line.split(":")
+      if(parsing.length == 10){
+        var alimento = new Alimento(parsing(1), parsing(4).toDouble, parsing(3).toDouble, parsing(2), parsing(6).toDouble, parsing(5).toDouble, parsing(7))
+        alimento.setAgrandado(parsing(9))
+        alimento.setRestricciones(parsing(8))
+        pedidos = (parsing(0), alimento)::pedidos
+      }
+      else{
+        var util = new Util(parsing(1), parsing(4).toDouble, parsing(3).toDouble, parsing(2))
+        pedidos = (parsing(0), util)::pedidos
+      }
+    }
+    source.close
+
+    return pedidos
+  }
+
+  def descargarHistorial(nombreSucursal : String) : List[(String, Double, Double)] = {
+    var historial : List[(String, Double, Double)] = Nil
+    var source = Source.fromFile("DB/historiales/"+ nombreSucursal + ".txt")
+    var lines = source.getLines
+    while(lines.hasNext){
+      val aux  = lines.next.split(":")
+      historial = (aux(0), aux(1).toDouble, aux(2).toDouble)::historial
+    }
+    source.close
+
+    return historial
+  }
+
+}
+
+
+
+class Sucursal(nombreSucursal : String){
+  private var db = new DB
   private var _precioDomicilio : Double = 3000.0
-  private var _sede : String = sede
-
-
-  //Se extrae el historial de la base de datos dependiendo de la sede
-  private var _historial : List[(String, Double, Double)] = Nil
-  var source = Source.fromFile("DB/historiales/"+ _sede + ".txt")
-  var lines = source.getLines
-  while(lines.hasNext){
-    val aux  = lines.next.split(":")
-    _historial = (aux(0), aux(1).toDouble, aux(2).toDouble)::_historial
-  }
-  source.close
-
-
-  //Se extraen los insumos de la base de datos y se guarda en la lista de insumos
-  private var _insumos: List[Insumo] = Nil
-  source = Source.fromFile("DB/insumos.txt")
-  lines = source.getLines
-  var aux : List[String] = lines.toList
-  for(line <- aux){
-    var parsing = line.split(":")
-    if(parsing.length == 6){
-      var alimento = new Alimento(parsing(0), parsing(3).toDouble, parsing(2).toDouble, parsing(1), parsing(5).toDouble, parsing(4).toDouble)
-      _insumos = alimento::_insumos
-    }
-    else{
-      var util = new Util(parsing(0), parsing(3).toDouble, parsing(2).toDouble, parsing(1))
-      _insumos = util::_insumos
-    }
-  }
-
+  private var _nombreSucursal : String = nombreSucursal
+//  private var _caja = new Caja(nombreSucursal)
+  private var _historial : List[(String, Double, Double)] = db.descargarHistorial(_nombreSucursal)
+  private var _insumos: List[Insumo] = db.descargarInsumos()
 
 
   //Setters y getters
@@ -41,7 +107,7 @@ class Sucursal(sede : String){
   def setPrecioDomicilio(precio : Double) : Unit = _precioDomicilio = precio
   def getHistorial() : List[(String, Double, Double)] = _historial
   def setHistorial(hist : List[(String, Double, Double)]) : Unit = _historial = hist
-  def getSede() : String = _sede
+  def getnombreSucursal() : String = _nombreSucursal
   def setInsumos(ins : List[Insumo]) : Unit = _insumos = ins
   def getInsumos() : List[Insumo] = _insumos
 
@@ -80,10 +146,12 @@ abstract class Insumo(codigo : String, costoVenta : Double, costoProduc : Double
   def getLabel(label : String) = _label = label
 }
 
-class Alimento(codigo : String, costoVenta : Double, costoProduc : Double, label : String, costoVentaAg : Double, costoProducAg : Double) extends Insumo(codigo, costoVenta, costoProduc, label){
+class Alimento(codigo : String, costoVenta : Double, costoProduc : Double, label : String, costoVentaAg : Double, costoProducAg : Double, categoria : String) extends Insumo(codigo, costoVenta, costoProduc, label){
   private var _restricciones : String = _
+  private var _categoria : String = _
   private var _costoVentaAgrandado : Double = costoVentaAg
   private var _costoProducAgrandano : Double = costoProducAg
+  private var _agrandado : String = _
 
   def setRestricciones(restricciones : String) = _restricciones = restricciones
   def getRestricciones = _restricciones
@@ -91,11 +159,53 @@ class Alimento(codigo : String, costoVenta : Double, costoProduc : Double, label
   def getCostoVentaAgrandado = _costoVentaAgrandado
   def setCostoProducAgrandado(costo : Double) = _costoProducAgrandano = costo
   def getCostoProducAgrandado = _costoVentaAgrandado
+  def getCategoria = _categoria
+  def serCategoria(categoria : String) = _categoria = categoria
+  def setAgrandado(p : String) = _agrandado = p
+  def getAgrandado : String = _agrandado
 }
 
 class Util(codigo : String, costoVenta : Double, costoProduc : Double, label : String) extends Insumo(codigo, costoVenta, costoProduc, label){
+}
+
+class Usuario(usuario : String, constrasena : String){
+  private var _usuario : String = usuario
+  private var _contrasena : String = constrasena
+
+  def getUsuario = _usuario
+  def getContrasena = _contrasena
+  def setUsuario(usuario : String) = _usuario = usuario
+  def setConstrasena(contrasena: String) = _contrasena = contrasena
+}
+
+class Cliente(usuario : String, constrasena : String, edad : String, sexo : String, telefono : String, cedula : String) extends Usuario(usuario, constrasena){
+  private var _edad : String = edad
+  private var _sexo : String = sexo
+  private var _telefono : String = telefono
+  private var _cedula : String = cedula
+
+  def setEdad(edad : String) = _edad = edad
+  def setSexo(sexo : String) = _sexo = sexo
+  def setTelefono(telefono : String) = _telefono = telefono
+  def setCedula(cedula : String) = _cedula = cedula
+  def getEdad() = _edad
+  def getSexo() = _sexo
+  def getTelefono() = _telefono
+  def getCedula() = _cedula
 
 }
+
+/*
+class Caja(nombreSucursal : String){
+  private var _nombreSucursal : String = nombreSucursal
+  private var _catalogo : List[Alimento] = descargarAlimento()
+  private var _pedidos : List[(String, Insumo)] = descargarPedidos(_nombreSucursal)
+
+  def agregarPedido(insumo : Insumo, cedula : String) : Boolean = {
+
+  }
+*/
+
 
 
 object Pruebas{
